14e0c0a8dae5f9d879e5f841738e1eaee0aa3554
IsraaAbdElnaby <israa.tobgy@gmail.com>
2019-10-27 22:28:11 +0200

    Adding menu icon and navigation events for the drawer

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,27 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import { Navigation } from 'react-native-navigation';
 import {RecyclerListView, DataProvider, LayoutProvider} from 'recyclerlistview';
 
 class HomeScreen extends Component {
+    constructor(props){
+        super(props);
+        Navigation.events().bindComponent(this);
+        // this.props.navigator.setOnNavigationEvent(this.navigationButtonPressed);
+    }
+    navigationButtonPressed = ({ buttonId }) => {
+        const { componentId } = this.props;
+      
+        if (buttonId === 'sideMenu') {
+          Navigation.mergeOptions(componentId, {
+            sideMenu: {
+              right: {
+                visible: true,
+              },
+            },
+          });
+        }
+      }
     render(){
         return(
             <View>
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,34 +1,46 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startMainTabs = () => {
-    Navigation.setRoot({
-        root: {
-            sideMenu: {
-                right: {
-                    component: {
-                        name: "task.SideDrawer"
-                    }
-                },
-                center: {
-                    stack: {
-                        // children:[{
+    Promise.all([
+        Icon.getImageSource("ios-menu", 30),
+    ]).then(([menuIcon]) => {
+        Navigation.setRoot({
+            root: {
+                sideMenu: {
+                    right: {
                         component: {
-                            name: 'task.HomeScreen'
-                        },
-                        // }],
-                        options: {
-                            topBar: {
-                                rightButtons: [{
-                                    text: 'كل الخدمات',
-                                    color: '#FAC858'
-                                }]
-                            }
+                            name: "task.SideDrawer"
                         }
                     },
+                    center: {
+                        stack: {
+                            children: [{
+                                component: {
+                                    name: 'task.HomeScreen'
+                                },
+                            }],
+                            options: {
+                                topBar: {
+                                    rightButtons: [
+                                        // {
+                                        //     text: 'كل الخدمات',
+                                        //     color: '#FAC858'
+                                        // },
+                                        {
+                                            id: 'sideMenu',
+                                            icon: menuIcon
+                                        }
+                                    ]
+                                }
+                            }
+                        },
+                    }
                 }
             }
-        }
-    });
+        });
+    })
+
 };
 
 export default startMainTabs;
